af0154d741ce6249bed0beb71d7a54d632bee1a4
Andrei Rus <andrei_rus23@yahoo.com>
2022-01-27 09:53:09 +0200

    Final project

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+function compute() {
+  y = new Date().getFullYear();
+  p = document.getElementById("principal").value;
+  n = document.getElementById("years").value;
+  r = document.getElementById("rate").value;
+  result = document.getElementById("result");
+
+  principcipaText.innerHTML = "If you deposit ";
+  principalNumber.innerHTML = p;
+  commaText.innerHTML = ",";
+  rateText.innerHTML = "at an interest rate of ";
+  rateNumber.innerHTML = r;
+  percentText.innerHTML = "%.";
+  resltText.innerHTML = " You will receive an amount of ";
+  result.innerHTML = (p * n * r) / 100;
+  commaText2.innerHTML = ",";
+  var convertNumber = parseInt(n);
+  var resultYers = y + convertNumber;
+  yearText.innerHTML = " in the year ";
+  numberOfYears.innerHTML = resultYers;
 }
-        
